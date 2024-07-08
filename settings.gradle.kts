2c5a51cf65907d9c3c4f459fc995eef698178ce3
Hxncus <Aika0281@gmail.com>
2024-07-08 14:58:00 +0500

    1.1.0 Changed project structure Relocated src/ into bukkit/src/ Fixed local libraries

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+rootProject.name = "FastPluginConfigurer"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+        maven("https://repo.mineinabyss.com/releases")
+    }
+}
+
+dependencyResolutionManagement {
+    // configures repositories for all projects
+    repositories {
+        maven("https://repo.papermc.io/repository/maven-public/")
+        maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://jitpack.io")
+        mavenLocal()
+    }
+}
+
+include("bukkit")
